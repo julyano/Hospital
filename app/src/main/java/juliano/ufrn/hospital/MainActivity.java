9209c0a34a10ab50320c84af015d2f9e6496c4b3
julyano <juliano@dimap.ufrn.br>
2016-11-29 08:21:27 -0300

    Teste

---
+++ app/src/main/java/juliano/ufrn/hospital/MainActivity.java
@@ -0,0 +1,35 @@
+package juliano.ufrn.hospital;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnLigarSensores = (Button) findViewById(R.id.btnLigaSensor);
+        Button btnDesligarSensores = (Button) findViewById(R.id.btnDesligaSensor);
+
+        btnLigarSensores.setOnClickListener( new View.OnClickListener(){
+             @Override
+             public void onClick(View view) {
+                 startService(new Intent(getBaseContext(),Sensores.class));
+             }
+         });
+
+        btnDesligarSensores.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopService(new Intent(getBaseContext(), Sensores.class));
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/juliano/ufrn/hospital/Sensores.java
@@ -0,0 +1,30 @@
+package juliano.ufrn.hospital;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.widget.Toast;
+
+/**
+ * Created by juliano on 29/11/16.
+ */
+public class Sensores extends Service {
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Toast.makeText(this,"Sensores Ligados", Toast.LENGTH_LONG).show();
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this,"Sensores Desligados", Toast.LENGTH_LONG).show();
+    }
+}
