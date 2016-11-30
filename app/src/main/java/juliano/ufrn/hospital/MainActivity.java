58a90e8a902105ef47079c633b6a265f357b0d40
julyano <juliano@dimap.ufrn.br>
2016-11-30 18:56:31 +0000

    volley 2

--- app/src/main/java/juliano/ufrn/hospital/MainActivity.java
+++ app/src/main/java/juliano/ufrn/hospital/MainActivity.java
@@ -5,9 +5,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,10 +36,84 @@ public class MainActivity extends AppCompatActivity {
         btnLigarSensores.setOnClickListener( new View.OnClickListener(){
              @Override
              public void onClick(View view) {
-                 startService(new Intent(getApplicationContext(),Sensores.class));
+                 //startService(new Intent(getApplicationContext(),Sensores.class));
+
+                 //Toast.makeText(this,"Sensores Ligados", Toast.LENGTH_LONG).show();
+
+                 JSONObject metadatasElements = new JSONObject();
+                 final JSONObject objMain = new JSONObject();
+                 try {
+                     metadatasElements.put("name","location");
+                     metadatasElements.put("type","string");
+                     metadatasElements.put("value","WGS84");
+
+                     JSONArray aMeta = new JSONArray();
+                     aMeta.put(metadatasElements);
+
+                     //JSONObject mainMeta = new JSONObject();
+                     //mainMeta.put("metadatas",aMeta);
+
+                     JSONArray aAttr = new JSONArray();
+
+                     JSONObject nomeElements = new JSONObject();
+                     nomeElements.put("name","nome");
+                     nomeElements.put("type","text");
+                     nomeElements.put("value","paciente720");
+
+                     JSONObject positionElements = new JSONObject();
+                     positionElements.put("name","position");
+                     positionElements.put("type","geo:point");
+                     positionElements.put("value","-5.79448,-35.211");
+                     positionElements.put("metadatas",aMeta);
+
+                     JSONObject pressaoElements = new JSONObject();
+                     pressaoElements.put("name","pressao");
+                     pressaoElements.put("type","integer");
+                     pressaoElements.put("value","paciente720");
+
+                     JSONObject batimento_cardiacoElements = new JSONObject();
+                     batimento_cardiacoElements.put("name","batimento_cardiaco");
+                     batimento_cardiacoElements.put("type","integer");
+                     batimento_cardiacoElements.put("value","30");
+
+                     JSONObject nivel_glicoseElements = new JSONObject();
+                     nivel_glicoseElements.put("name","nivel_glicose");
+                     nivel_glicoseElements.put("type","integer");
+                     nivel_glicoseElements.put("value","30");
+
+
+                     aAttr.put(nomeElements);
+                     aAttr.put(positionElements);
+                     aAttr.put(pressaoElements);
+                     aAttr.put(batimento_cardiacoElements);
+                     aAttr.put(nivel_glicoseElements);
+
+                     JSONObject cElements = new JSONObject();
+                     cElements.put("id","paciente720");
+                     cElements.put("type","Paciente");
+                     cElements.put("isPattern","false");
+                     cElements.put("attributes",aAttr);
+
+
+                     objMain.put("contextElements",cElements);
+                     objMain.put("updateAction","UPDATE");
+
+
+
+
+                 } catch (JSONException e) {
+                     e.printStackTrace();
+                 }
+
+                 String url = "http:177.20.147.208/v1/updateContext";
+
 
 
              }
+
+
+
+
          });
 
         btnDesligarSensores.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/juliano/ufrn/hospital/Sensores.java
+++ app/src/main/java/juliano/ufrn/hospital/Sensores.java
@@ -9,11 +9,15 @@ import android.widget.Toast;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONTokener;
@@ -38,6 +42,8 @@ import static java.lang.Math.round;
  */
 public class Sensores extends Service {
 
+
+
     @Override
     public IBinder onBind(Intent intent) {
         return null;
@@ -46,6 +52,150 @@ public class Sensores extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Toast.makeText(this,"Sensores Ligados", Toast.LENGTH_LONG).show();
+
+        JSONObject metadatasElements = new JSONObject();
+        final JSONObject objMain = new JSONObject();
+        try {
+            metadatasElements.put("name","location");
+            metadatasElements.put("type","string");
+            metadatasElements.put("value","WGS84");
+
+            JSONArray aMeta = new JSONArray();
+            aMeta.put(metadatasElements);
+
+            //JSONObject mainMeta = new JSONObject();
+            //mainMeta.put("metadatas",aMeta);
+
+            JSONArray aAttr = new JSONArray();
+
+            JSONObject nomeElements = new JSONObject();
+            nomeElements.put("name","nome");
+            nomeElements.put("type","text");
+            nomeElements.put("value","paciente720");
+
+            JSONObject positionElements = new JSONObject();
+            positionElements.put("name","position");
+            positionElements.put("type","geo:point");
+            positionElements.put("value","-5.79448,-35.211");
+            positionElements.put("metadatas",aMeta);
+
+            JSONObject pressaoElements = new JSONObject();
+            pressaoElements.put("name","pressao");
+            pressaoElements.put("type","integer");
+            pressaoElements.put("value","paciente720");
+
+            JSONObject batimento_cardiacoElements = new JSONObject();
+            batimento_cardiacoElements.put("name","batimento_cardiaco");
+            batimento_cardiacoElements.put("type","integer");
+            batimento_cardiacoElements.put("value","30");
+
+            JSONObject nivel_glicoseElements = new JSONObject();
+            nivel_glicoseElements.put("name","nivel_glicose");
+            nivel_glicoseElements.put("type","integer");
+            nivel_glicoseElements.put("value","30");
+
+
+            aAttr.put(nomeElements);
+            aAttr.put(positionElements);
+            aAttr.put(pressaoElements);
+            aAttr.put(batimento_cardiacoElements);
+            aAttr.put(nivel_glicoseElements);
+
+            JSONObject cElements = new JSONObject();
+            cElements.put("id","paciente720");
+            cElements.put("type","Paciente");
+            cElements.put("isPattern","false");
+            cElements.put("attributes",aAttr);
+
+
+            objMain.put("contextElements",cElements);
+            objMain.put("updateAction","UPDATE");
+
+
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        String url = "http:177.20.147.208/v1/updateContext";
+        /*JSONTokener tok = new JSONTokener(objMain);
+        JSONObject jsObj = new JSONObject();
+        try {
+            jsObj = (JSONObject) tok.nextValue();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }*/
+
+       /* HashMap<String, String> headers = new HashMap<String, String>();
+        headers.put("Content-Type", "application/json");
+       */
+
+        /*@Override
+        public void onResponse(JSONObject response) {
+            Log.i("teste", response.toString());
+            //pDialog.hide();
+        }*/
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        JsonObjectRequest jsonObjRequest = new JsonObjectRequest
+                (Request.Method.POST, url, objMain, new Response.Listener<JSONObject>()
+                {
+                    @Override
+                    public void onResponse(JSONObject response)
+                    {
+                        Toast.makeText(getApplicationContext(), response.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                },
+                        new Response.ErrorListener()
+                        {
+                            @Override
+                            public void onErrorResponse(VolleyError error)
+                            {
+                                Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+
+        /*JsonArrayRequest jsonObjReq = null;
+        jsonObjReq = new JsonArrayRequest(Request.Method.POST, url, new JSONObject(headers),
+                new Response.Listener<JSONArray>() {
+
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.i("teste", response.toString());
+                    }
+
+
+                }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.d("teste", "Error: " + error.getMessage());
+                //pDialog.hide();
+            }
+        });*/
+
+        /*{
+
+            *//**
+             * Passing some request headers
+             * *//*
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                headers.put("Content-Type", "application/json");
+                return headers;
+            }
+
+        };*/
+
+        //AppController.getInstance().addToRequestQueue(request);
+        queue.add(jsonObjRequest);
+
+
+        /*
 
         new Thread(new Runnable(){
             @Override
@@ -89,6 +239,10 @@ public class Sensores extends Service {
                         Log.i("PACIENTE_MORRENDO",s);
                     }
                 }
+
+
+
+
 
                 String object =
                         "{  \"contextElements\":\n" +
@@ -135,49 +289,10 @@ public class Sensores extends Service {
 
 
 
-                String url = "http:177.20.147.208/v1/updateContext";
-                JSONTokener tok = new JSONTokener(object);
-                try {
-                    JSONObject jsObj = (JSONObject) tok.nextValue();
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-
-
-                JsonObjectRequest jsonObjReq = null;
-                jsonObjReq = new JsonObjectRequest(Request.Method.POST,
-                        url, null,
-                        new Response.Listener<JSONObject>() {
-
-                            @Override
-                            public void onResponse(JSONObject response) {
-                                Log.i("teste", response.toString());
-                                //pDialog.hide();
-                            }
-                        }, new Response.ErrorListener() {
-
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        VolleyLog.d("teste", "Error: " + error.getMessage());
-                        //pDialog.hide();
-                    }
-                }) {
-
-                    /**
-                     * Passing some request headers
-                     * */
-                    @Override
-                    public Map<String, String> getHeaders() throws AuthFailureError {
-                        HashMap<String, String> headers = new HashMap<String, String>();
-                        headers.put("Content-Type", "application/json");
-                        return headers;
-                    }
-
-                };
 
 
 
-                /*URL url = null;
+                URL url = null;
                 try {
                     url = new URL(null);
                 } catch (MalformedURLException e) {
@@ -205,9 +320,10 @@ public class Sensores extends Service {
                     e.printStackTrace();
                 } finally {
                     conn.disconnect();
-                }*/
+                }
             }
         }).start();
+        */
         return START_STICKY;
     }
 
